6e5801b7ec21b72ee49a15b4f7544dc7a50cd005
pentester <python.malaysia.life@mail.ru>
2025-03-10 22:18:41 +0300

    Added Ip internal list for debug toolbar

--- Shop/settings.py
+++ Shop/settings.py
@@ -56,7 +56,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-INTERNAL_IPS = ['*']
+INTERNAL_IPS = ['127.0.0.1']
+import socket
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+
+
+
+
 
 
 ROOT_URLCONF = 'Shop.urls'
